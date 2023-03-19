e3c5fc606fb21481f11c1a577dec8827a18fa45b
Ivan Semenov <Nikman040800@yandex.ru>
2023-03-19 11:37:59 +0300

    Insert hw 3

---
+++ Insert.sql
@@ -0,0 +1,308 @@
+insert into performers_albums(performer_id, album_id)
+values(1, 1);
+
+insert into albums(name, year_of_issue)
+values('Loud', 2020);
+
+insert into tracks(name, duration, album)
+values('Whats My name', '00:04:23', 2);
+
+insert into tracks(name, duration, album)
+values('Skin', '00:05:04', 2);
+
+insert into performers_albums(performer_id, album_id)
+values(2, 2);
+
+insert into albums(name, year_of_issue)
+values('The Fame', 2008);
+
+insert into tracks(name, duration, album)
+values('Just Dance', '00:04:02', 3);
+
+insert into tracks(name, duration, album)
+values('Paparazzi', '00:03:28', 3);
+
+insert into performers_albums(performer_id, album_id)
+values(3, 3);
+
+insert into albums(name, year_of_issue)
+values('Relapse', 2009);
+
+insert into tracks(name, duration, album)
+values('Hello', '00:04:08', 4);
+
+insert into tracks(name, duration, album)
+values('We Made You', '00:04:29', 4);
+
+insert into performers_albums(performer_id, album_id)
+values(4, 4);
+
+insert into albums(name, year_of_issue)
+values('My world 2.0', 2010);
+
+insert into tracks(name, duration, album)
+values('Baby', '00:03:36', 5);
+
+insert into tracks(name, duration, album)
+values('Up', '00:03:55', 5);
+
+insert into performers_albums(performer_id, album_id)
+values(5, 5);
+
+insert into albums(name, year_of_issue)
+values('Soul Revolution', 1971);
+
+insert into tracks(name, duration, album)
+values('Soul Rebel', '00:03:19', 6);
+
+insert into tracks(name, duration, album)
+values('Kaya', '00:02:39', 6);
+
+insert into performers_albums(performer_id, album_id)
+values(6, 6);
+
+insert into albums(name, year_of_issue)
+values('Together Brothers', 1974);
+
+insert into tracks(name, duration, album)
+values('Alive and Well', '00:01:15', 7);
+
+insert into tracks(name, duration, album)
+values('You Gotta Case', '00:01:26', 7);
+
+insert into performers_albums(performer_id, album_id)
+values(7, 7);
+
+insert into albums (name, year_of_issue)
+values('#7', 1986);
+
+insert into tracks(name, duration, album)
+values('Deep Water', '00:02:31', 8);
+
+insert into tracks(name, duration, album)
+values('Cow Town', '00:02:49', 8);
+
+insert into compilations(name, date)
+values('pop compilation', '01/01/2020');
+
+insert into performers_albums(performer_id, album_id)
+values(8, 8);
+
+insert into compilations(name, date)
+values('RnB compilation', '03/05/2020');
+
+insert into compilations(name, date)
+values('star compilation', '01/09/2010');
+
+insert into compilations(name, date)
+values('rhytm and blues compilation', '07/11/1975');
+
+insert into compilations(name, date)
+values('women star compilation', '10/06/2020');
+
+insert into compilations(name, date)
+values('american compilation', '01/01/2010');
+
+insert into compilations(name, date)
+values('afroamerican compilation', '02/02/2020');
+
+insert into compilations(name, date)
+values('all in one', '31/12/2020');
+
+insert into compilations_tracks(compilation_id, track_id)
+values(1, 1);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(1, 2);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(1, 3);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(1, 4);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(1, 5);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(1, 6);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(1, 7);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(1, 10);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(1, 11);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(2, 4);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(2, 5);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(2, 10);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(2, 11);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(3, 4);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(3, 5);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(3, 6);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(3, 7);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(3, 8);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(3, 9);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(3, 10);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(3, 11);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(4, 1);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(4, 2);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(4, 3);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(4, 14);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(4, 15);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(5, 4);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(5, 5);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(5, 6);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(5, 7);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(6, 1);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(6, 2);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(6, 3);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(6, 6);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(6, 7);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(6, 8);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(6, 9);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(6, 14);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(6, 15);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(6, 16);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(6, 17);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(7, 1);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(7, 2);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(7, 3);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(7, 4);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(7, 12);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(7, 13);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(7, 14);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(7, 15);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 1);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 2);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 3);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 4);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 5);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 6);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 7);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 8);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 9);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 10);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 11);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 12);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 13);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 14);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 15);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 16);
+
+insert into compilations_tracks(compilation_id, track_id)
+values(8, 17);
